9dbe3ff793acee7a72b13b02d49c5ec611837ada
sandesh hegde <sandeshhegde1@gmail.com>
2017-09-25 10:00:26 -0700

    V1.0 Initial physics

---
+++ Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour {
+
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		scoreText.text = "No collision";
+	}
+
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Wire")) {
+
+			scoreText.text = "Collision Detected";
+			Handheld.Vibrate ();
+
+		}
+
+	}
+
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.CompareTag ("Wire")) {
+
+			scoreText.text = "No collision";
+
+		}
+
+	}
+}
