e5251296fe44a689a1d2860eb549aeda601d7f0a
HEric120301 <qingfenh@usc.edu>
2017-10-13 16:29:40 -0700

    add main menu

---
+++ Assets/Scene_/NewBehaviourScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+import UnityEngine;
+import UnityEngine.SceneManagement;
+
+function NewGame() {
+	SceneManager.LoadScene(1);	
+//	SceneManager.UnloadSceneAsync(0);
+}
+
+function Settings() {
+	SceneManager.LoadScene(2);
+
+}
+
+function About() {
+	SceneManager.LoadScene(3);
+
+}
+
+function MainMenu() {
+	SceneManager.LoadScene(0);
+//	SceneManager.UnloadSceneAsync(2);
+
+}
+
