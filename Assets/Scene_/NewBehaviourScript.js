7bde04f6cee4db5c5ab016ae2f1a0f98d897fc83
taofangcx <31911352+taofangcx@users.noreply.github.com>
2017-10-15 11:56:24 -0700

    background of main menu

--- Assets/Scene_/NewBehaviourScript.js
+++ Assets/Scene_/NewBehaviourScript.js
@@ -1,26 +1,27 @@
 ﻿#pragma strict
 
-import UnityEngine;
 import UnityEngine.SceneManagement;
 
 function NewGame() {
-	SceneManager.LoadScene(1);	
+	SceneManager.LoadScene(1);
+//	SceneManager.LoadScene(2);	
+		
 //	SceneManager.UnloadSceneAsync(0);
 }
 
 function Settings() {
-	SceneManager.LoadScene(2);
-
-}
-
-function About() {
 	SceneManager.LoadScene(3);
 
 }
 
-function MainMenu() {
+function About() {
+	SceneManager.LoadScene(4);
+}
+
+function MainMemu() {
 	SceneManager.LoadScene(0);
 //	SceneManager.UnloadSceneAsync(2);
 
 }
+
 
--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -6,11 +6,26 @@ using UnityEngine.UI;
 public class CollisionScript : MonoBehaviour {
 
 	public Text scoreText;
+	public Text healthText;
+	public Text starText;
+	private int count;
+	private int starsCount;
+	private GameObject[] stars;
 
 	// Use this for initialization
 	void Start () {
 		// scoreText.text = "No collision";
 		scoreText.text = "Start from the Start point!";
+		healthText.text = "Heart * Three";
+		starText.text = "Stars : 0";
+		count = 3;
+		starsCount = 0;
+		GameObject.FindGameObjectWithTag("Wire").SetActive(true);
+		stars = GameObject.FindGameObjectsWithTag("Star");
+		foreach (GameObject star in stars)
+		{
+			star.SetActive(true);
+		}
 	}
 
 
@@ -19,7 +34,30 @@ public class CollisionScript : MonoBehaviour {
 
 			scoreText.text = "Collision Detected";
 			Handheld.Vibrate ();
+			count--;
+			switch(count) {
+			case 2  :
+				healthText.text = "Health * Two";
+				break; /* optional */
+			case 1 :
+				healthText.text = "Health * One";
+				break; /* optional */
+			case 0 :
+				healthText.text = "You die";
+				scoreText.text = "Game over";
+				GameObject.FindGameObjectWithTag("Wire").SetActive(false);
+				stars = GameObject.FindGameObjectsWithTag("Star");
+				foreach (GameObject star in stars)
+				{
+					star.SetActive(false);
+				}
+				break; /* optional */
+			}
+		}
 
+		if (other.gameObject.CompareTag ("Star")) {
+			starsCount++;
+			starText.text = "Stars : " + starsCount;
 		}
 
 		if (other.gameObject.CompareTag ("StartCube")) {
